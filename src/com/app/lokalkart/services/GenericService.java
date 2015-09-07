00f2474e85a49763a1e8ef04decf7f9bef5eeb3a
rohitlokalkart <rohit@lokalkart.com>
2015-09-07 22:58:48 +0530

    Committing Generic Service interface

--- src/com/app/lokalkart/services/GenericService.java
+++ src/com/app/lokalkart/services/GenericService.java
@@ -1,7 +1,59 @@
+/*
+ * 
+ */
 package com.app.lokalkart.services;
 
 import java.io.Serializable;
+import java.util.List;
 
+/**
+ * 
+ * @author Rohit Ranjan
+ *
+ * @param <T>
+ * @param <PK>
+ */
 public interface GenericService<T, PK extends Serializable> {
+	
+	/**
+	 * 
+	 * @return
+	 */
+    List<T> getAll();
+    
+    /**
+     * 
+     * @param id
+     * @return
+     */
+    T get(PK id);
+    
+    /**
+     * 
+     * @param id
+     * @return
+     */
+    boolean exists(PK id);
+    
+    /**
+     * 
+     * @param object
+     * @return
+     */
+    T save(T object);
+    
+    /**
+     * 
+     * @param id
+     */
+    void remove(PK id);
+    
+    /**
+     * 
+     * @param searchTerm
+     * @param clazz
+     * @return
+     */
+    List<T> search(String searchTerm, Class<?> clazz);
 
 }
